12b36e7595724504fc9eb5e62926b6774e03bd73
Yahiya Muhammed <muhammedyahiya715@gmail.com>
2020-11-19 22:32:22 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 var imported = document.createElement("script");
-imported.src = "https://mhdyahiya.github.io/a.js";
+imported.src = "https://tender-fermat-a049de.netlify.app/b.js";
 document.getElementsByTagName("head")[0].appendChild(imported);
 
 function thirdJS() { alert("third from https://mhdyahiya.github.io/a.js"); }
