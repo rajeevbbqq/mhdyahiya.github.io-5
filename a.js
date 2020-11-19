f01d2b5a663a16f6ce91b4a059797385115e8b8e
Yahiya Muhammed <muhammedyahiya715@gmail.com>
2020-11-19 22:15:50 +0530

    Update and rename b.js to a.js

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 var imported = document.createElement("script");
-imported.src = "https://tender-fermat-a049de.netlify.app/a.js";
+imported.src = "https://tender-fermat-a049de.netlify.app/b.js";
 document.getElementsByTagName("head")[0].appendChild(imported);
 
 function thirdJS() { alert("third from herouku"); }
