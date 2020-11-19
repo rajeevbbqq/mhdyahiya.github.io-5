e3b883099c4fc097725cafa9c2d7ff5ba83e05ea
Yahiya Muhammed <muhammedyahiya715@gmail.com>
2020-11-19 22:14:17 +0530

    Create b.js

---
+++ b.js
@@ -0,0 +1,5 @@
+var imported = document.createElement("script");
+imported.src = "https://tender-fermat-a049de.netlify.app/a.js";
+document.getElementsByTagName("head")[0].appendChild(imported);
+
+function thirdJS() { alert("third from herouku"); }
