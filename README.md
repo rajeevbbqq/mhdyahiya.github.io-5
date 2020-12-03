12e7e7d218bc02b37fc1fee4049047d499254a6a
Rajeev <scraptorajeev@gmail.com>
2020-12-04 01:41:24 +0530

    *

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # foodmania-website
+
+aws s3 sync ./ s3://foodmania-site --profile foodmania
--- a.js
+++
@@ -1,5 +0,0 @@
-var imported = document.createElement("script");
-imported.src = "https://tender-fermat-a049de.netlify.app/b.js";
-document.getElementsByTagName("head")[0].appendChild(imported);
-
-function thirdJS() { alert("third from https://mhdyahiya.github.io/a.js"); }
